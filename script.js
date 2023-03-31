b6231a544c1b7fcd730d070ef65df1624453edef
NiyuShion <126596226+NiyuShion@users.noreply.github.com>
2023-03-31 19:20:40 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,57 @@
-import React, { useEffect, useRef } from 'react';
-import Flickity from 'flickity';
+var options = {
+  accessibility: true,
+  prevNextButtons: true,
+  pageDots: true,
+  setGallerySize: false,
+  arrowShape: {
+    x0: 10,
+    x1: 60,
+    y1: 50,
+    x2: 60,
+    y2: 45,
+    x3: 15
+  }
+};
 
-function MyCarousel() {
-  const carouselRef = useRef(null);
-  const slidesRef = useRef([]);
+var carousel = document.querySelector('[data-carousel]');
+var slides = document.getElementsByClassName('carousel-cell');
+var flkty = new Flickity(carousel, options);
 
-  useEffect(() => {
-    const options = {
-      accessibility: true,
-      prevNextButtons: true,
-      pageDots: true,
-      setGallerySize: false,
-      arrowShape: {
-        x0: 10,
-        x1: 60,
-        y1: 50,
-        x2: 60,
-        y2: 45,
-        x3: 15
-      }
-    };
+flkty.on('scroll', function () {
+  flkty.slides.forEach(function (slide, i) {
+    var image = slides[i];
+    var x = (slide.target + flkty.x) * -1/3;
+    image.style.backgroundPosition = x + 'px';
+  });
+});
 
-    const flkty = new Flickity(carouselRef.current, options);
+// Получаем элементы DOM
+const menuToggle = document.querySelector('.menu-toggle');
+const categories = document.querySelector('.categories');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-    function handleScroll() {
-      flkty.slides.forEach(function (slide, i) {
-        var image = slidesRef.current[i];
-        var x = (slide.target + flkty.x) * -1/3;
-        image.style.backgroundPosition = x + 'px';
-      });
-    }
+// Обрабатываем клик по кнопке меню
+menuToggle.addEventListener('click', function() {
+  mobileMenu.classList.toggle('show');
+  categories.classList.toggle('hide');
+});
 
-    flkty.on('scroll', handleScroll);
-
-    return () => {
-      flkty.off('scroll', handleScroll);
-      flkty.destroy();
-    }
-  }, []);
-
-  return (
-    <div className="carousel" data-carousel ref={carouselRef}>
-      {slides.map((slide, i) => (
-        <div className="carousel-cell" key={i} ref={el => slidesRef.current[i] = el}>
-          {   <div class="hero-slider" data-carousel>
-	<div class="carousel-cell" style="background-image: url(img/img-1.jpg);">
-		<div class="overlay"></div>
-		<div class="inner">
-			<h3 class="subtitle">Экология</h3>
-			<h2 class="title">Как часть нашей жизни</h2>
-			<a href="#" class="btn">Подробнее</a>
-		</div>
-	</div>
-
-	<div class="carousel-cell" style="background-image: url(img/img-2.jpg);">
-		<div class="overlay"></div>
-		<div class="inner">
-			<h3 class="subtitle">экология</h3>
-			<h2 class="title">это ответственность каждого из нас</h2>
-			<a href="#" class="btn">Подробнее</a>
-		</div>
-	</div>
-
-	<div class="carousel-cell" style="background-image: url(img/img-3.jpg);">
-		<div class="overlay"></div>
-		<div class="inner">
-			<h3 class="subtitle">экология</h3>
-			<h2 class="title">то, чего мы должны достичь</h2>
-			<a href="#" class="btn">Подробнее</a>
-		</div>
-	</div>
-</div>}
-        </div>
-      ))}
-    </div>
-  );
+// Добавляем класс "hide" к категориям на мобильных устройствах
+function checkWidth() {
+  if (window.innerWidth < 768) {
+    categories.classList.add('hide');
+    menuToggle.style.display = 'block';
+  } else {
+    categories.classList.remove('hide');
+    menuToggle.style.display = 'none';
+    mobileMenu.classList.remove('show');
+  }
 }
+
+// Вызываем функцию при загрузке страницы и при изменении размера окна
+window.addEventListener('load', checkWidth);
+window.addEventListener('resize', checkWidth);
+
+
+
+
